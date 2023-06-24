24eb96a9c9b112d436b4a6104b699c663d373090
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-06-24 19:44:34 +0530

    style:sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import RocketOutlinedIcon from "@mui/icons-material/RocketOutlined";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="sidebar">
+        <div className="side-box">
+          <HomeOutlinedIcon fontSize="large" /> <h5>Home</h5>
+        </div>
+        <div className="side-box">
+          <RocketOutlinedIcon fontSize="large" /> <p>Explore</p>
+        </div>
+        <div className="side-box">
+          <BookmarkBorderOutlinedIcon fontSize="large" /> <p>Bookmarks</p>
+        </div>
+        <div className="side-box">
+          <AccountCircleOutlinedIcon fontSize="large" /> <p>Profile</p>
+        </div>
+        <div className="footer">
+          <div className="profile">
+            <div className="img"></div>
+            <div><h4>Tanay Pratap</h4>
+            <p>@tanaypratap</p></div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
